fc8b4f62bf45860ee2055f397b87a49a99ab5513
Tindyy <vdgraaf.tessa@gmail.com>
2023-09-08 09:37:37 +0200

    "start over"
    HTML:
    - renamed article section
    - removed button
    - removed text (p & d1)
    
    CSS:
    - changed colors (background & card).
    - added a different image
    - updated CSS file to match HTML file
    
    JS:
    - Add Tilt effect

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,17 @@
-const expandButton = document.querySelector('header button')
-expandButton.addEventListener('click', expand)
+const card = document.querySelector(".card");
 
-function expand () {
-  document.body.classList.toggle('expand')
+card.addEventListener("mousemove", cardMouseMove);
+
+function cardMouseMove(event){
+const cardWidth = card.offsetWidth;
+const cardHeight = card.offsetHeight;
+const centerX = card.offsetLeft + cardWidth/2;
+const centerY = card.offsetTop + cardHeight/2;
+const mouseX = event.clientX - centerX;
+const mouseY = event.clientY - centerY;
+const rotateX = ((-1)*25*mouseY/(cardHeight/2)).toFixed(2);
+const rotateY = (25*mouseX/(cardWidth/2)).toFixed(2);
+
+card.style.transform = `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
 }
+
